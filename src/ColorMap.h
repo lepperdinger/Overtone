7f1055a3a7af33c3c597b383529334aeeb9f1ee5
Stefan Lepperdinger <lepperdinger.stefan@gmail.com>
2022-03-12 17:06:44 +0100

    Added comments to ColorMap.

--- src/ColorMap.h
+++ src/ColorMap.h
@@ -11,17 +11,49 @@
 class ColorMap
 {
 public:
+  /**
+   * This constructor exists to be able to get the theme names without being
+   * forced to specify a certain theme, e.g.,  ColorMap ().get_theme_names ().
+   */
   ColorMap ();
-  ColorMap (std::string color_map_name, double gain, double gate);
+
+  /**
+   * @param theme color theme
+   * @param gain multiplies each key of the keyboard by this value
+   * @param gate all keys below this threshold are set to 0
+   */
+  ColorMap (std::string theme, double gain, double gate);
+
+  /**
+   * Converts a input_value to a RGB color.
+   * @param input_value
+   * @return color = { red, green, blue }
+   */
   std::vector<unsigned char> operator() (double input_value);
+
+  /**
+   * @return edge color = { red, green, blue }
+   */
   std::vector<unsigned char> get_edge_color ();
+
+  /**
+   * @return names of all available themes
+   */
   std::vector<std::string> get_theme_names ();
+
   static void run_tests ();
 
 private:
+  // each key of the keyboard is being multiplied by this value
   double gain;
+
+  // all keys below this threshold are set to 0
   double gate;
+
   std::string theme;
+
+  std::vector<double> limits;
+
   void initialize_themes ();
   bool check_if_theme_exists (const std::string &theme_name);
   std::unordered_map<std::string, std::vector<std::vector<unsigned char> > >
@@ -30,7 +62,6 @@ private:
   static double linear_interpolation (std::vector<double> lower_point,
                                       std::vector<double> upper_point,
                                       double x_value);
-  std::vector<double> limits;
 
   static std::vector<unsigned char>
   hex_string_to_rgb_values (const std::string &hex_string);
