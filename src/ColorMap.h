818726b39ad3d80be96cba815111b17e578887cd
Stefan Lepperdinger <lepperdinger.stefan@gmail.com>
2022-03-12 17:33:33 +0100

    Added further comments to ColorMap.

--- src/ColorMap.h
+++ src/ColorMap.h
@@ -52,13 +52,20 @@ private:
 
   std::string theme;
 
+  // corresponding values of the colors of the theme
+  // (darkest color = 0., brightest color = 1.)
   std::vector<double> limits;
 
-  void initialize_themes ();
-  bool check_if_theme_exists (const std::string &theme_name);
   std::unordered_map<std::string, std::vector<std::vector<unsigned char> > >
       color_maps;
+
   std::unordered_map<std::string, std::vector<unsigned char> > edge_colors;
+
+  void initialize_themes ();
+
+  bool check_if_theme_exists (const std::string &theme_name);
+
+
   static double linear_interpolation (std::vector<double> lower_point,
                                       std::vector<double> upper_point,
                                       double x_value);
@@ -66,9 +73,17 @@ private:
   static std::vector<unsigned char>
   hex_string_to_rgb_values (const std::string &hex_string);
 
+  /**
+   * @param string_color_map hex strings of the colors, e.g., { "03fa24", ... }
+   * @return RGB values of the colors: { { red, green, blue }, ... }
+   */
   static std::vector<std::vector<unsigned char> >
   convert_color_map (const std::vector<std::string> &string_color_map);
 
+  /**
+   * @param input_value
+   * @return RGB values { red, green, blue}
+   */
   std::vector<unsigned char> evaluate_color (double input_value);
 
   void determine_limits ();
