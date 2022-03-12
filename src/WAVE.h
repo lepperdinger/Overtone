565fbf8992cd1395be12a28649b58f4a113f5729
Stefan Lepperdinger <lepperdinger.stefan@gmail.com>
2022-03-12 20:04:14 +0100

    Added the license.

--- src/WAVE.h
+++ src/WAVE.h
@@ -1,6 +1,25 @@
-//
-// Created by stefan on 8/15/21.
-//
+/******************************************************************************
+
+    Overtone: A Music Visualizer
+
+    WAVE.h
+
+    Copyright (C) 2022 Stefan Lepperdinger
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+******************************************************************************/
 
 #ifndef OVERTONE_WAVE_H
 #define OVERTONE_WAVE_H
